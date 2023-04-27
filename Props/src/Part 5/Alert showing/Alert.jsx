e309329de3af85fdc0ba5092038bada2fda81821
Pooja Kumari <86198133+Poojakpph@users.noreply.github.com>
2023-04-27 20:06:47 +0530

    Create Alert.jsx

---
+++ Props/src/Part 5/Alert showing/Alert.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Alert(props) {  
+
+    const capitalize=(word)=>{  // this function makes 's' in "success" capital
+        const lower= word.toLowerCase();
+        return lower.charAt(0).toUpperCase()+ lower.slice(1);   
+    }
+
+  return (
+    // the below syntax tells that if(props.alert!=NULL) then only other element (i.e. after && ) will be evaluated  OR first props.alert 
+    //is evaluated and if it returns True then only expression after && will be evaluated
+     props.alert &&  <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+        <strong>{ capitalize(props.alert.type) }</strong> : {props.alert.msg}
+     </div>
+
+  )
+}
+
+export default Alert
